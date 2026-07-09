8c0d62746599ca518887b5bfd33caa9c23af37c8
C-Ezra-M <70766223+C-Ezra-M@users.noreply.github.com>
2026-07-09 22:41:08 +0200

    actually add the filters %)

--- filters.js
+++ filters.js
@@ -5,6 +5,22 @@ export function fmtDate(/** @type Date */ date) {
     }).format(date)
 }
 
+export function fmtFullDate(/** @type Date */ date) {
+    return new Intl.DateTimeFormat("en-US", {
+        timeZone: "Europe/Warsaw",
+        dateStyle: "full",
+        timeStyle: "short",
+    }).format(date)
+}
+
 export function sortExp(/** @type any[] */ array, /** @type string */ ...exp) {
     return array.toSorted(new Function(...exp))
+}
+
+export function jsonParse(input) {
+    return JSON.parse(input)
+}
+
+export function toDateObject(input) {
+    return new Date(input)
 }
