e397e2e1313db96d17ed97810753ada0ade7e682
C-Ezra-M <70766223+C-Ezra-M@users.noreply.github.com>
2026-08-23 00:58:35 +0200

    some more stuff

--- _config.ts
+++ _config.ts
@@ -1,9 +1,13 @@
 import lume from "lume/mod.ts";
+import plaintext from "lume/plugins/plaintext.ts";
+import metas from "lume/plugins/metas.ts";
 
 const site = lume({
     location: new URL("https://cezram.xyz"),
 });
 
 site.add("static", ".")
+site.use(plaintext())
+site.use(metas())
 
 export default site;
