740281740d141a81f6310cb55cc3791ddb694b36
C-Ezra-M <70766223+C-Ezra-M@users.noreply.github.com>
2026-08-29 02:16:48 +0200

    modify config and template

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import lume from "lume/mod.ts";
 import plaintext from "lume/plugins/plaintext.ts";
 import metas from "lume/plugins/metas.ts";
 import date from "lume/plugins/date.ts";
+import slugifyUrls from "lume/plugins/slugify_urls.ts";
 
 const site = lume({
     location: new URL("https://cezram.xyz"),
@@ -13,8 +14,15 @@ site.use(metas())
 site.use(date({
     formats: {
         "FULL_DATE": "PPPP",
-        "FULL_DATETIME": "PPPPpp"
+        "FULL_DATETIME": "PPPPpp",
     }
 }))
+site.use(slugifyUrls())
+
+site.ignore("rarezen")
+
+site.preprocess([".md"], (pages) => {
+    pages.forEach((page) => page.data.templateEngine = ["vto", "md"])
+});
 
 export default site;
